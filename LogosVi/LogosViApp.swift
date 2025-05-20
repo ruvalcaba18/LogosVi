1b7f0cba984dedad14de200373de32cfe6562fc3
Jael Ruvalcaba <Jael.ruvalcaba.contractor@realtor.com>
2025-05-20 09:06:24 -0700

    Initial Commit

---
+++ LogosVi/LogosViApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogosViApp.swift
+//  LogosVi
+//
+//  Created by JaelWizeline on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LogosViApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
